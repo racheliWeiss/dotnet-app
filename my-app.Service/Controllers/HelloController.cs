52ecb113e4ac69150d6c7c97921f47f3becc2ea9
Racheli weiss <75696575+racheliWeiss@users.noreply.github.com>
2024-06-03 12:50:11 +0000

    hello

--- my-app.Service/Controllers/HelloController.cs
+++ my-app.Service/Controllers/HelloController.cs
@@ -19,7 +19,7 @@ namespace my_app.Service.Controllers
         public IActionResult Thank()
         {
            HttpClient httpClient = new HttpClient();
-           var result = httpClient.GetAsync("https://user4-donet-app.user4-application.cluster.local.svc/redhat/redhat");      
+           var result = httpClient.GetAsync("https://user4-donet-app.user4-application.cluster.local.svc/hello/redhat");      
             return Ok("Thanks Rivka: " + result);
         }
     }
